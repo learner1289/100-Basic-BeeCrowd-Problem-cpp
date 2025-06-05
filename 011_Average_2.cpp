a8744e787610d39d6d393f004b3056c4d4122384
learner1289 <saha2305341289@diu.edu.bd>
2025-06-05 22:03:40 +0600

    Create 011_Average_2.cpp

---
+++ 011_Average_2.cpp
@@ -0,0 +1,8 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    double A,B,C,average;
+    cin>>A>>B>>C;
+    average=(A*2 + B*3 + C*5)/10.0;
+    printf("MEDIA = %0.1lf\n",average);
+}
