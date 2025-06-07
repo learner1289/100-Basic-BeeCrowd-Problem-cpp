463820de622db13c5dd0bca111d6beb47e126bec
learner1289 <saha2305341289@diu.edu.bd>
2025-06-07 22:06:58 +0600

    014_Sphere.cpp

---
+++ 014_Sphere.cpp
@@ -0,0 +1,8 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    double R,pi= 3.14159,volume;
+    cin>>R;
+    volume = (4.0/3) * pi * (R*R*R);
+    printf("VOLUME = %0.3lf\n", volume);
+}
