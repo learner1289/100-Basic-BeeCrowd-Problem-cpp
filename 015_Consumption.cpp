19a28500e1f870065b25715df9d2d47cc9896323
learner1289 <saha2305341289@diu.edu.bd>
2025-06-08 14:55:38 +0600

    https://judge.beecrowd.com/en/problems/view/1014

---
+++ 015_Consumption.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int X;
+    float Y,average_consump;
+    cin>>X>>Y;
+    average_consump = (X/Y);
+    printf("%0.3f km/l\n",average_consump);
+}
