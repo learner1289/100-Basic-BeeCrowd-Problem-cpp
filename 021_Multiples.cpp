33127920f9a51ab86309ba29d71b4529f8706ad6
learner1289 <saha2305341289@diu.edu.bd>
2025-06-09 22:43:15 +0600

    Create 021_Multiples.cpp
    https://judge.beecrowd.com/en/problems/view/1044

---
+++ 021_Multiples.cpp
@@ -0,0 +1,8 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int A,B;
+    cin>>A>>B;
+    if(A%B==0 || B%A==0) printf("Sao Multiplos\n");
+    else printf("Nao sao Multiplos\n");
+}
